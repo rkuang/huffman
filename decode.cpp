0e61581827ac1789f6567da8c0bde9a0ae26d9e5
Ricky Kuang <rkuang26@gmail.com>
2018-04-16 18:25:17 -0700

    encode and decode finished

--- decode.cpp
+++ decode.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <fstream>
+#include <map>
 
 using namespace std;
 
@@ -7,15 +9,41 @@ int main(int argc, char const *argv[]) {
   if (argc != 2) {
     cerr << "Program failed to run.\n";
     cerr << "Usage: ./decode codewords.txt\n";
+    exit(1);
+  }
+
+  string file_name = argv[1];
+  ifstream codewords_txt (file_name);
+  if (!codewords_txt) {
+    cerr << "Program failed to load " << file_name << endl;
+    exit(1);
   }
 
   // Parse codewords.txt
   // Put character-codeword pairs into map
   // Key = Codeword, Value = Character
+  string ch, code;
+  map <string, string> codewords;
+  while (codewords_txt >> ch >> code) {
+    codewords[code] = ch;
+  }
 
   // Read stdin
   // Read character by character, appending to a current code variable
   // While reading, check map. If key is found, decode to value.
+  string line;
+  while (getline(cin, line)) {
+    code = "";
+    for (int i=0; i<line.length(); i++) {
+      // cout << line[i] << endl;
+      code += line[i];
+      // cout << code << endl;
+      if (codewords.find(code) != codewords.end()) {
+        cout << (char)stoi(codewords[code]);
+        code = "";
+      }
+    }
+  }
 
   return 0;
 }
--- encode.cpp
+++ encode.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <fstream>
+#include <map>
 
 using namespace std;
 
@@ -7,14 +9,39 @@ int main(int argc, char const *argv[]) {
   if (argc != 2) {
     cerr << "Program failed to run.\n";
     cerr << "Usage: ./encode codewords.txt\n";
+    exit(1);
+  }
+
+  string file_name = argv[1];
+  ifstream codewords_txt (file_name);
+  if (!codewords_txt) {
+    cerr << "Program failed to load " << file_name << endl;
+    exit(1);
   }
 
   // Parse codewords.txt
   // Put character-codeword pairs into map
   // Key = Character, Value = Codeword
+  string ch, code;
+  map <string, string> codewords;
+  while (codewords_txt >> ch >> code) {
+    codewords[ch] = code;
+  }
+
+  // map <string, string> :: iterator itr;
+  // for (itr = codewords.begin(); itr != codewords.end(); itr++) {
+  //   cout  << itr->first << '\t' << itr->second << endl;
+  // }
 
   // Read stdin
   // Convert chars to ASCII values, find character in map, print code to stdout
+  string line;
+  while (getline(cin, line)) {
+    for (int i=0; i<line.length(); i++) {
+      cout << codewords[to_string((int)line[i])];
+    }
+    cout << codewords[to_string((int)'\n')];
+  }
 
   return 0;
 }
